070049dee9a5f00fd260311406f08dea91a2ad29
Unknown <salufie@gmail.com>
2018-09-14 09:43:58 +1000

    Setting up the skeleton of the app

---
+++ src/loans/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Nav from 'reactstrap/lib/Nav';
+import NavItem from 'reactstrap/lib/NavItem';
+import NavLink from 'reactstrap/lib/NavLink';
+import { Link } from 'react-router-dom';
+import '../css/App.css';
+
+//Header for application
+export default class Header extends React.Component {
+    render() {
+        var url = window.location.pathname;
+        var urlsplit = url.split("/").slice(-1)[0];
+
+        var isActive1 = urlsplit === 'events' ? 'is-active': '';
+        var isActive2 = urlsplit === 'user-management' ? 'is-active': '';
+
+        return (
+            <div className="event-title">
+                <h3>{this.props.adminName}</h3>
+                <Nav>
+                    <NavItem>
+                        <NavLink className={isActive1} tag={Link} to="/events">Events</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className={isActive2} tag={Link} to="/user-management">Teams</NavLink>
+                    </NavItem>
+                </Nav>
+            </div>
+        );
+    }
+}
