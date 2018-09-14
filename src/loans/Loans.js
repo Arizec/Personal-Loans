269356057099b83f0cbb4b3077ed66285a03eb45
Unknown <salufie@gmail.com>
2018-09-14 14:22:23 +1000

    Form is more stable

--- src/loans/Loans.js
+++ src/loans/Loans.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import '../css/App.css';
 import Header from "./Header";
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
 const background = require('../assets/beach.png');
+
 
 //Shows all admin users that can currently manage the event
 //Allows main admin to add other admin or invite attendee users
@@ -11,12 +16,23 @@ export default class Loans extends React.Component {
         return (
             <div>
                 <Header/>
-                <div className={"admin-events"}>
-                    <div className="container">
+                <div className={"contain"}>
                         <img className={"homepage-background"} src={background} alt="" />
-                            <div className="centered">Centered</div>
+                    <div className={"centered"}>
+                        <ListGroup>
+                            <ListGroupItem>Cras justo odio</ListGroupItem>
+                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                            <ListGroupItem>Morbi leo risus</ListGroupItem>
+                            <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                        </ListGroup>
                     </div>
                 </div>
+
+                    {/*<div className="container">*/}
+                        {/*<img className={"homepage-background"} src={background} alt="" />*/}
+                            {/*<div className="centered">hi</div>*/}
+                    {/*</div>*/}
             </div>
 
         );
